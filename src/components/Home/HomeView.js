d778e831011efa51f84b81f8c9647730cd7cf3c8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-27 12:39:34 +0300

    useAsyncState

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import i18n from '../../i18n/en';
 import styles from './HomeViewStyles';
 
 const HomeView = () => {
+  const [state, setState] = useState({
+    time: 0,
+  });
+
+  const renderStartButton = () => {
+    return (
+      <TouchableOpacity onPress={() => {
+        console.log(state.time, '');
+        setInterval(() => {
+          console.log(state.time);
+          setState({ time: state.time + 1000 });
+        }, 1000);
+      }} style={styles.mainActionButton}>
+        <Text style={styles.mainActionButtonText}>{i18n.HOME.START}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderRunningTimer = () => {
+    return (
+      <TouchableOpacity onPress={() => {}} style={styles.mainActionButton}>
+        <Text style={styles.mainActionButtonText}>{state.time}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ flex: 1 }}>
         <Text style={styles.welcomeHeader}>{i18n.HOME.WELCOME_HEADER}</Text>
       </View>
       <View style={{ flex: 2 }}>
-        <TouchableOpacity onPress={() => {}} style={styles.mainActionButton}>
-          <Text style={styles.mainActionButtonText}>{i18n.HOME.START}</Text>
-        </TouchableOpacity>
+        {state.time > 0 ? renderRunningTimer() : renderStartButton()}
       </View>
     </View>
   );
