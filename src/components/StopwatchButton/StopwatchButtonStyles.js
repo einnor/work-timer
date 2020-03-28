3c57a9b60f65846e2232ec8f0344e7d0bb179442
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-28 10:39:50 +0300

    Added styles for button component

---
+++ src/components/StopwatchButton/StopwatchButtonStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  mainActionButton: {
+    width: 284,
+    height: 284,
+    borderRadius: 142,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00cd5e',
+  },
+  mainActionButtonText: {
+    fontSize: 50,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  mainActionButtonPauseText: {
+    fontSize: 24,
+  }
+});
+
+export default styles;
