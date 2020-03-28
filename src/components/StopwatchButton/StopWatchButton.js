724d1aebe5960820fc01c60f41a1b39e5b8a01a4
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-28 10:36:15 +0300

    Refactored buttons into their own component

---
+++ src/components/StopwatchButton/StopWatchButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import moment from 'moment';
+
+import i18n from '../../i18n/en';
+import styles from './HomeViewStyles';
+
+const StopwatchButton = ({ time, paused, onStart, onPause }) => {
+
+  if (time > 0) {
+    return (
+      <TouchableOpacity onPress={onPause} style={styles.mainActionButton}>
+        <Text style={styles.mainActionButtonText}>{moment.utc(time).format('HH:mm:ss')}</Text>
+        <Text style={[styles.mainActionButtonText, styles.mainActionButtonPauseText]}>{i18n.HOME.PAUSE}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <TouchableOpacity onPress={onStart} style={styles.mainActionButton}>
+      <Text style={styles.mainActionButtonText}>{i18n.HOME.START}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default StopwatchButton;
