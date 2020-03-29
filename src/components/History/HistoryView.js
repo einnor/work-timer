4deb28c772d1aac8ce38e7fec90c039b4036ce51
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-29 19:00:47 +0300

    History View

---
+++ src/components/History/HistoryView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HistoryView = () => {
+  return (
+    <View>
+      <Text>History</Text>
+    </View>
+  );
+};
+
+export default HistoryView;
+
