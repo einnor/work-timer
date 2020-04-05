c20178537f4b9311c73816c02eef5abbb2749fe8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-05 10:33:38 +0300

    Render activities

--- src/components/History/HistoryView.js
+++ src/components/History/HistoryView.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, AsyncStorage } from 'react-native';
 
 const HistoryView = () => {
+  const [activities, setActivities] = useState([]);
+  const getActivities = async () => {
+    const activities = JSON.parse(await AsyncStorage.getItem('@activities')) || [];
+    setActivities(activities);
+  };
+
+  useEffect(() => {
+    getActivities();
+  }, [])
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <View>
+          <Text>{DataTransferItem.name}</Text>
+        </View>
+      </View>
+    );
+  };
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <>
+      <Text>Saved Activities</Text>
+      <FlatList
+        data={activities}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `${item.name.replace(/\s/g, '-')}-${index}`}
+      />
+    </>
   );
 };
 
