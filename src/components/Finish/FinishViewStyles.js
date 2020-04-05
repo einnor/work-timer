0a956d3157debc4ea8a53da3a963a6df03f88628
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-05 11:50:54 +0300

    Finish styles

---
+++ src/components/Finish/FinishViewStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 0.8,
+	},
+	innerContainer: {
+		flex: 4,
+		justifyContent: 'space-between',
+	},
+});
+
+export default styles;
