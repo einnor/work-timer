32f27d426d26d7416415d155a99b324bdc232700
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-06 07:07:03 +0300

    Added action button styles

---
+++ src/components/ActionButton/ActionButtonStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	touchableStyle: {
+		width: 134,
+		height: 44,
+		borderRadius: 15,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	captionStyle: {
+		fontSize: 24,
+		textTransform: 'lowercase'
+	}
+});
+
+export default styles;
