7c8bcb4b7d6b156720d5731246f62ac9681a5a98
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-06 06:50:38 +0300

    Updated finish view styles

--- src/components/Finish/FinishViewStyles.js
+++ src/components/Finish/FinishViewStyles.js
@@ -8,6 +8,31 @@ const styles = StyleSheet.create({
 		flex: 4,
 		justifyContent: 'space-between',
 	},
+	header: {
+		fontSize: 40,
+		textAlign: 'center',
+		flex: 2,
+	},
+	subHeader: {
+		fontSize: 40,
+		textAlign: 'center',
+		flex: 2,
+	},
+	activityNameLabel: {
+		fontSize: 16,
+	},
+	activityNameInput: {
+		borderRadius: 5,
+		borderColor: '#848484',
+		borderWidth: 1,
+		height: 44,
+		marginTop: 7
+	},
+	actionButtonsContainer: {
+		marginTop: 65,
+		flexDirection: 'row',
+		justifyContent: "space-between"
+	},
 });
 
 export default styles;
