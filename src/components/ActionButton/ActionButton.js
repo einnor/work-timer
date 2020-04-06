5185439b019766ebec0443fb16b85f14ae86a01c
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-06 07:09:20 +0300

    Finished action button component

--- src/components/ActionButton/ActionButton.js
+++ src/components/ActionButton/ActionButton.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
+import styles from './ActionButtonStyles';
+
 const ActionButton = ({ label, onPress, textColor, backgroundColor }) => {
-    return (
-        <TouchableOpacity
-          onPress={onPress}
-        >
-            <Text>{label}</Text>
-        </TouchableOpacity>
-    );
+	return (
+		<TouchableOpacity
+			onPress={onPress}
+			style={[styles.touchableStyle, { backgroundColor }]}
+		>
+			<Text style={[styles.captionStyle, { color: textColor }]}>{label}</Text>
+		</TouchableOpacity>
+	);
 };
 
 export default ActionButton;
