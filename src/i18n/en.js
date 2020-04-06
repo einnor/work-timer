e7997307a56e1469a545cb3ed9ac829269460754
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-06 06:48:18 +0300

    Üpdated i18n-en

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,8 +1,11 @@
 const i18n = {
   TIME_FORMAT: 'HH:mm:ss',
   DATE_FORMAT: 'DD MMM YYYY',
+  CANCEL: 'Cancel',
+  SAVE: 'Save',
   HOME: {
     WELCOME_HEADER: 'Good Morning!',
+    FINISH_BTN_CAPTION: 'finish',
     START: 'START',
     PAUSE: 'PAUSE',
     PAUSED: 'PAUSED',
@@ -12,8 +15,13 @@ const i18n = {
     SAVED_ACTIVITIES: 'Saved Activities',
   },
   FINISH: {
-    MAIN_HEADER: '',
-  }
+    MAIN_HEADER: 'You just spent',
+    ACTIVITY_NAME_LABEL: 'Activity name',
+  },
+  STOP_WATCH: {
+    START: 'START',
+    PAUSE: 'PAUSE'
+},
 };
 
 export default i18n;
